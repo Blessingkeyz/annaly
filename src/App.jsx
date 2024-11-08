6d071a66951e72d127410deb320939d55371534f
Blessingkeyz <umukoroblessing@gmail.com>
2024-11-08 12:30:24 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,14 @@ import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-// import DidYouKnow from "./components/DidYouKnow";
-// import FAQSection from "./components/FAQSection";
-// import Features from "./components/Features";
-// import Footer from "./components/Footer";
-// import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      {/* <Features />
-      <DidYouKnow />
-      <Testimonials />
-      <FAQSection />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,95 +1,38 @@
+import footerImg from "/footerimg.jpg";
+
 const Footer = () => {
   return (
-    <footer className="  py-8">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-4">
-        {/* First Column: About */}
-        <div className="ml-12">
-          {/* Logo */}
-          <img src="/logo.png" alt="WM Logo" className="h-20 ml-5" />
-          <p>
-            Shop with World Mart <br /> For quality products
+    <footer className="bg-[#051d40]">
+      <div className="flex px-10 py-20 justify-center items-center">
+        <div className="">
+          <p className="text-[#f1945B]   text-[79.8px] leading-tight">
+            ANNALY TRADE <br /> CAPITAL
           </p>
+          <p className="text-[#4bd1fb] text-[34px] ">Connect with us</p>
+          <div>
+            <i className="fas text-2xl mr-5 fa-globe my-10 text-white"></i>
+            <a
+              href="https://app.atcapital.us"
+              className="text-[#ff6100] text-2xl"
+            >
+              https://app.atcapital.us
+            </a>
+          </div>
+          <div>
+            <i className="fas text-2xl mr-5 fa-envelope  text-white mb-10"></i>
+            <a
+              href="mailto:support@atcapital.us"
+              className="text-[#ff6100] text-2xl"
+            >
+              support@atcapital.us
+            </a>
+          </div>
         </div>
 
-        {/* Second Column: Useful Links */}
-        <div>
-          <h3 className="text-xl font-bold mb-4">Useful Links</h3>
-          <ul className="space-y-4">
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Features
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Blogs
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                FAQ
-              </a>
-            </li>
-          </ul>
+        {/* image*/}
+        <div className=" block">
+          <img src={footerImg} alt="footerimg" className="w-[500px]" />
         </div>
-
-        {/* Third Column: Menu */}
-        <div>
-          <h3 className="text-xl font-bold mb-4">Main Menu</h3>
-          <ul className="space-y-4">
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Offers
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Products
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        {/* Fourth Column: Contact */}
-        <div>
-          <h3 className="text-xl font-bold mb-4">Contact Us</h3>
-          <a href="mailto:worldmart@gmail.com">worldmart@gmail.com</a>
-          <p className="mt-4">
-            <a href="tel:+64958248966">+64 958 248 966</a>
-          </p>
-          <div className="mt-4 space-x-4">Social media</div>
-        </div>
-      </div>
-
-      <div className="text-center mt-8 space-x-4 text-sm text-gray-400">
-        <a href="#" className="hover:text-gray-400">
-          <i className="fa-brands fa-facebook"></i>
-        </a>
-        <a href="#" className="hover:text-gray-400">
-          <i className="fa-brands fa-instagram"></i>
-        </a>
-        <a href="#" className="hover:text-gray-400">
-          <i className="fa-brands fa-twitter"></i>
-        </a>
-        <a href="#" className="hover:text-gray-400">
-          <i className="fa-brands fa-youtube"></i>
-        </a>
-        <span>Copyright © 2023 Dscode | All rights reserved</span>
       </div>
     </footer>
   );
